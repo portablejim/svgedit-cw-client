bb16c34ca9b5a45ac7a4fe267e0eacaa03ca8af2
portablejim <dcseee-github@yahoo.com.au>
2015-06-13 14:09:38 +1000

    Initial commit

---
+++ src/collabWhiteboard-extension.js
@@ -0,0 +1,11 @@
+
+svgEditor.addExtension("collabWhiteboardClient", function(methods) {
+    return {
+        name: "collabWhiteboardClient",
+        buttons: [],
+        elementChanged: function(elems) {
+            console.log("Element changed")
+            console.log(elems)
+        }
+    }
+}
